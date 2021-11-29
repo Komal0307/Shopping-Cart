2ca2de26e1ed1d633d3fdde863f4622475704e05
Komal0307 <komalwaykos24@gmail.com>
2021-11-29 17:23:45 +0530

    first commit

---
+++ my-app/src/Data.js
@@ -0,0 +1,34 @@
+import img1 from './img4.jpeg';
+import img2 from './img2.jpg';
+import img3 from './img3.jpg'
+
+const data = {
+     productData:[
+         {
+             id: 4,
+             img: img1,
+             title: 'Sweatshirts for Women',
+             desc: '',
+             price: 1000,
+         },
+         {
+             id: 2,
+             img: img2,
+             title: 'Casual wear, Part wear Tops for Women',
+             desc: '',
+             price: 700,
+         },
+         {
+             id: 3,
+             img: img3,
+             title: 'Cotton Tees WinterT-Shirt for Women',
+             desc: '',
+             price: 800,
+         },
+
+     ],
+
+
+};
+
+export default data;
